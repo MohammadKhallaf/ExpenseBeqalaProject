e255bc867bed7958a0ecda61424de4076883b203
MennaEmadZ <menna.e.zahran@gmail.com>
2022-02-22 00:27:52 +0200

    edit in APIs

--- cart/models.py
+++ cart/models.py
@@ -3,30 +3,32 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from accounts.models import *
 from store.models import *
+
 # Create your models here.
 User = get_user_model()
-class CheckOut(models.Model):
 
+
+class CheckOut(models.Model):
     # orderDate updated when done checkout
     STATE_CHOICES = (
-    ('open','OPEN'),
-    ('pending', 'PENDING'),
-    ('done','DONE'),
+        ('open', 'OPEN'),
+        ('pending', 'PENDING'),
+        ('done', 'DONE'),
     )
     user = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE)
-    store = models.ForeignKey(Store,to_field='id', on_delete=models.CASCADE, related_name='store')
+    store = models.ForeignKey(Store, to_field='id', on_delete=models.CASCADE, related_name='store')
     orderDate = models.DateTimeField(null=True)
     state = models.CharField(max_length=8, choices=STATE_CHOICES, default='open')
 
     def __str__(self):
         return f"{self.user} - {self.store} - {self.id}"
+
+
 class Cart(models.Model):
 
-
     order = models.ForeignKey(CheckOut, on_delete=models.CASCADE, related_name='carts')
-    product = models.ForeignKey(ProductPrice,to_field='id', on_delete=models.CASCADE, related_name='cartProduct')
+    product = models.ForeignKey(ProductPrice, to_field='id', on_delete=models.CASCADE, related_name='cartProduct')
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return f"{self.order} - {self.product} - {self.quantity}"
-
