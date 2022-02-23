2ad4962fa01d326986d771c1481e617d964d3470
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-23 12:15:57 +0200

    update cart views

---
+++ UserDashboard/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+UserDashboard Models
+- get and view all user data (consumer)
+- edit specified data:
+    - phone
+    - orders history
+    - checkouts
+    - preferred language (select [arabic,english])
+
+"""
+
+class UserProfile(models.Model):
+    pass
--- product_list/models.py
+++ product_list/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-import store.models
 
 # Create your models here.
 class Brand(models.Model):
@@ -14,14 +13,16 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name   
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    brand = models.ForeignKey(Brand,to_field='id', on_delete = models.CASCADE) 
-    category = models.ForeignKey(Category, to_field='id', on_delete = models.CASCADE) 
+    brand = models.ForeignKey(Brand, to_field='id', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, to_field='id', on_delete=models.CASCADE)
+
     # image = models.ImageField()
-    
+
     def __str__(self):
         return f"{self.name} - {self.description} - {self.brand}- {self.category}"
