f3a77eea79ca07d52ff3bafde13ba7a25b841aa1
MennaEmadZ <menna.e.zahran@gmail.com>
2022-02-25 22:14:51 +0200

    edits

--- store/urls.py
+++ store/urls.py
@@ -9,12 +9,10 @@ urlpatterns = [
     path('store/', views.storeApi),
     path('category/', views.storecategoryApi),
     path('prices/', views.priceApi),
-<<<<<<< HEAD
-   
-=======
+
     path('category/bakery/', views.bakeryApi),
     path('category/pharmacy/', views.pharmacyApi),
     path('category/market/', views.marketApi),
     path('store/<str:pk>/', views.storeSelect),
->>>>>>> eb563f90d3206b7085784afbc446c7b3a03e68ae
+
     ]
