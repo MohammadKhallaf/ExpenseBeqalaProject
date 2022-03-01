580dee632aff57c29a638370ebfc7482c7a7b343
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-03-02 01:38:08 +0200

    super_admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+    
+admin.site.register(StoreCategory)
+admin.site.register(Store)
+admin.site.register(ProductPrice)
+admin.site.register(ProductOffer)
