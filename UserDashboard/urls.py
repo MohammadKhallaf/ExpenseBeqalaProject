0a1ec2ef36f5afbb81059654427a15049a2e2bf2
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-03-06 16:13:06 +0200

    add user data update endpoint

--- UserDashboard/urls.py
+++ UserDashboard/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('open-stores/',views.getOpenCheckouts),
-    path('orders/',views.getAllOrders),
-    path('orders/<str:id>',views.getOrder),
+    path("open-stores/", views.getOpenCheckouts),
+    path("orders/", views.getAllOrders),
+    path("orders/<str:id>", views.getOrder),
+    path("update/", views.updateDetails)
 ]
